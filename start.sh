f803708e97e09603a0360fb283d837af85811cb5
wly <polaris.wanglingyu@hotmail.com>
2023-03-15 15:27:53 +0800

    Add slam-relative pull command

--- start.sh
+++ start.sh
@@ -8,6 +8,8 @@ cd .. # current directory is ~/YOUR_ws/src/xju-robot-noetic
 
 git clone https://github.com/ros/geometry2.git
 git clone https://github.com/Mr-Tony921/navigation.git
+git clone https://github.com/ros-perception/slam_gmapping.git
+git clone https://github.com/ros-perception/openslam_gmapping.git
 
 cd ..
 catkin_make
